f0745b40abde91d0da4b150e31c4daacabb33f1e
beessoft <dbo@beessoft.com>
2018-05-08 16:44:24 +0800

    Initial Commit

---
+++ dyyd/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  dyyd
+//
+//  Created by beessoft on 2018/5/8.
+//  Copyright © 2018年 beessoft. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ dyyd/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  dyyd
+//
+//  Created by beessoft on 2018/5/8.
+//  Copyright © 2018年 beessoft. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
